c565627c73c79de55e5576b3caffc60d3b156808
Samflame <63602085+Samflame@users.noreply.github.com>
2024-06-04 13:49:19 +0200

    Update if3.rs

--- exercises/03_if/if3.rs
+++ exercises/03_if/if3.rs
@@ -2,17 +2,17 @@
 //
 // Execute `rustlings hint if3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 pub fn animal_habitat(animal: &str) -> &'static str {
-    let identifier = if animal == "crab" {
+    let identifier: i32 = if animal == "crab" {
         1
     } else if animal == "gopher" {
-        2.0
+        2
     } else if animal == "snake" {
         3
     } else {
-        "Unknown"
+        0
     };
 
     // DO NOT CHANGE THIS STATEMENT BELOW
@@ -36,21 +36,23 @@ mod tests {
 
     #[test]
     fn gopher_lives_in_burrow() {
-        assert_eq!(animal_habitat("gopher"), "Burrow")
+        assert_eq!(animal_habitat("gopher"), "Burrow");
     }
 
     #[test]
     fn snake_lives_in_desert() {
-        assert_eq!(animal_habitat("snake"), "Desert")
+        assert_eq!(animal_habitat("snake"), "Desert");
     }
 
     #[test]
     fn crab_lives_on_beach() {
-        assert_eq!(animal_habitat("crab"), "Beach")
+        assert_eq!(animal_habitat("crab"), "Beach");
     }
 
     #[test]
     fn unknown_animal() {
-        assert_eq!(animal_habitat("dinosaur"), "Unknown")
+        assert_eq!(animal_habitat("dinosaur"), "Unknown");
     }
 }
+
+//l’erreur est liée au type de la variable identifier, j'ai choisi i32 car c'est un type entier correspondant aux valeurs utilisées
