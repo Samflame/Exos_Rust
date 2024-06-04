a17e5eec6d46354a013f62fbf9204d60c7417603
Samflame <63602085+Samflame@users.noreply.github.com>
2024-06-04 16:49:39 +0200

    Update cow1.rs

--- exercises/19_smart_pointers/cow1.rs
+++ exercises/19_smart_pointers/cow1.rs
@@ -12,7 +12,7 @@
 //
 // Execute `rustlings hint cow1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::borrow::Cow;
 
@@ -37,7 +37,7 @@ mod tests {
         let slice = [-1, 0, 1];
         let mut input = Cow::from(&slice[..]);
         match abs_all(&mut input) {
-            Cow::Owned(_) => Ok(()),
+            Cow::Owned(vec) => Ok(assert_eq!(*vec, vec![1, 0, 1])),
             _ => Err("Expected owned value"),
         }
     }
@@ -49,6 +49,8 @@ mod tests {
         let mut input = Cow::from(&slice[..]);
         match abs_all(&mut input) {
             // TODO
+            Cow::Borrowed(vec) => Ok(assert_eq!(*vec, vec![0, 1, 2])),
+            _ => Err("Expected borrowed value"),
         }
     }
 
@@ -61,6 +63,8 @@ mod tests {
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
             // TODO
+            Cow::Owned(vec) => Ok(assert_eq!(*vec, vec![0, 1, 2])),
+            _ => Err("Expected owned value"),
         }
     }
 
@@ -73,6 +77,8 @@ mod tests {
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
             // TODO
+            Cow::Owned(vec) => Ok(assert_eq!(*vec, vec![1, 0, 1])),
+            _ => Err("Expected owned value"),
         }
     }
 }
