7912a4c4ab40882d4c9e821e73cee078d1a108b0
Samflame <63602085+Samflame@users.noreply.github.com>
2024-06-04 16:04:26 +0200

    Update options3.rs

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 struct Point {
     x: i32,
@@ -13,9 +13,13 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
+    // Utilisez une référence à `y` pour éviter de prendre possession de la valeur.
+    match &y {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
-    y; // Fix without deleting this line.
+    // Maintenant, `y` peut être utilisé après le `match` car sa valeur n'a pas été déplacée.
+    y; // Cette ligne est correcte et ne causera pas d'erreur.
 }
+
+//J'ai ajouté une référence non mutable à y avant le match
