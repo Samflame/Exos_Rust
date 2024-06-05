c99c31e234ec3f4fd0ab8e94c8b6d04979095ee6
Samflame <63602085+Samflame@users.noreply.github.com>
2024-06-05 14:52:13 +0200

    Update traits4.rs

--- exercises/15_traits/traits4.rs
+++ exercises/15_traits/traits4.rs
@@ -7,9 +7,6 @@
 // Execute `rustlings hint traits4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM DONE
-
-
 pub trait Licensed {
     fn licensing_info(&self) -> String {
         "some information".to_string()
